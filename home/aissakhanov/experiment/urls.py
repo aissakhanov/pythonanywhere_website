6376d16848d4999824196c4b3bc53b1f6232358b
aissakhanov <alfarabi.issakhanov@gmail.com>
2022-06-15 00:20:07 +0600

    upload files

---
+++ home/aissakhanov/experiment/urls.py
@@ -0,0 +1,30 @@
+"""experiment URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('ml_algorithms/', views.ml_algorithms, name='ml_algorithms'),
+    path('num_methods/', views.num_methods, name='num_methods'),
+    path('supervised_learning/',views.supervised_learning, name='supervised_learning'),
+    path('linear_regression/',views.lin_reg, name='lin_reg'),
+    path('root_finding/', views.root_finding, name='root_finding'),
+    path('bis_method/', views.bis_method, name='bis_method'),
+]
---
+++ home/aissakhanov/experiment/views/index.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+
+def index(request):
+	context = {}
+	return render(request, 'index.html', context)
+
+def ml_algorithms(request):
+    context = {}
+    return render(request, 'ML_algorithms/0_ML_algorithms.html', context)
+
+def num_methods(request):
+	context = {}
+	return render(request, 'Numerical_methods/0_Num_methods.html', context)
+
+
+def supervised_learning(request):
+    context = {}
+    return render(request, 'ML_algorithms/1_Supervised_learning.html', context)
+
+def lin_reg(request):
+    context = {}
+    return render(request, 'ML_algorithms/1_1_Linear_regression.html', context)
+
+def root_finding(request):
+	context = {}
+	return render(request, 'Numerical_methods/1_1_Root_finding.html', context)
+def bis_method(request):
+	context = {}
+	return render(request, 'Numerical_methods/1_1_1_Bisection_method.html', context)
